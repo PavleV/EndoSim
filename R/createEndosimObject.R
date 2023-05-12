1b85463d68fadb14c44848765df737471ddf7517
Pavle Vrljicak <a007678@007678.local>
2023-05-12 16:27:17 +0100

    set class for EndoSim object

--- R/createEndosimObject.R
+++ R/createEndosimObject.R
@@ -1,10 +1,12 @@
-#
+# create EndoSim class
+
+setClass("EndoSim", slots=list(name="character", id="numeric", contact="character"))
 
 
 #' Create an EndoSim simulation object
 #'
 #' This function initialises an EndoSim object with initial settings. It requires age range and
-#' kepted.
+#' number of iterations.
 #'
 #' @param iter_num Number of iterations of the simulation
 #' @param age_range Range of ages
@@ -12,7 +14,8 @@
 #' @export
 createEndoSim <- function(iter_num=1000,age_range=20:45){
 
-  print("Creating EndoSim simulation object")
+  print("Created EndoSim simulation object")
+  new("EndoSim",name="Steven", id=1002, contact="West Avenue")
 
 }
 
