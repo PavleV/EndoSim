add1f7e003db6ff46909de144a0f802d8b4c857b
Pavle Vrljicak <a007678@007678.local>
2023-05-12 15:48:56 +0100

    populate description file

--- R/createEndosimObject.R
+++ R/createEndosimObject.R
@@ -1,5 +1,19 @@
-# This function initialises an EndoSim object with initial settings
+#
 
 
+#' Create an EndoSim simulation object
+#'
+#' This function initialises an EndoSim object with initial settings. It requires age range and
+#' kepted.
+#'
+#' @param iter_num Number of iterations of the simulation
+#' @param age_range Range of ages
+#' @return An EndoSim object
+#' @export
+createEndoSim <- function(iter_num=1000,age_range=20:45){
+
+  print("Creating EndoSim simulation object")
+
+}
 
 
