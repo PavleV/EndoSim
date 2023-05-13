367eeb5886219f22196b73e1bcdfa465f5e5eb03
Pavle Vrljicak <a007678@007678.local>
2023-05-13 20:23:02 +0100

    updated endosim class

--- R/createEndosimObject.R
+++ R/createEndosimObject.R
@@ -1,6 +1,23 @@
 # create EndoSim class
 
-setClass("EndoSim", slots=list(name="character", id="numeric", contact="character"))
+setClass("EndoSim", slots=list(iter_num="numeric",age_range="numeric",result="matrix"), validity = check_endosim)
+
+check_endosim <- function(object){
+
+  errors <- character()
+  age_range.length <- length(object@age_range)
+
+  if (age_range.length != 2){
+    msg <- paste0("Number of ages supplied is ",age_range.length,". Should be 2.")
+    errors <- c(errors, msg)
+  }
+
+  if (length(errors) == 0) TRUE else errors
+
+}
+
+
+
 
 
 #' Create an EndoSim simulation object
@@ -14,9 +31,17 @@ setClass("EndoSim", slots=list(name="character", id="numeric", contact="characte
 #' @export
 createEndoSim <- function(iter_num=1000,age_range=20:45){
 
-  print("Created EndoSim simulation object")
-  new("EndoSim",name="Steven", id=1002, contact="West Avenue")
+  new("EndoSim",iter_num=iter_num,age_range=age_range,result=matrix(0,2,2))
 
 }
 
+# create generic method
 
+setMethod("show",
+          "EndoSim",
+          function(object) {
+            cat("Number of iterations:",object@iter_num, "\n")
+            cat("Age range:",object@age_range, "\n")
+            #cat("Contact:", object@contact, "\n")
+          }
+)
