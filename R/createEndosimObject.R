417884d59d3b7ac316a2c6c10ddbf94d8822e4af
Pavle Vrljicak <a007678@007678.local>
2023-05-15 16:31:07 +0100

    update documentation

--- R/createEndosimObject.R
+++ R/createEndosimObject.R
@@ -16,7 +16,7 @@ check_endosim <- function(object){
 }
 
 
-setClass("EndoSim", slots=list(iter_num="numeric",age_range="numeric",result="matrix"), validity = check_endosim)
+setClass("EndoSim", slots=list(iter_num="numeric",age_range="numeric",simulation="list"), validity = check_endosim)
 
 
 
@@ -25,20 +25,24 @@ setClass("EndoSim", slots=list(iter_num="numeric",age_range="numeric",result="ma
 #' This function initialises an EndoSim object with initial settings. It requires age range and
 #' number of iterations.
 #'
-#' @param iter_num Number of iterations of the simulation
-#' @param age_range Range of ages
+#' @param iter_num Number of iterations of the simulation. Default value = 1000.
+#' @param age_range Range of ages. Default value = c(20,45).
+#' @param space_SI Number of cycles between successful implantations. Default value = 12.
 #' @return An EndoSim object
 #' @export
-createEndoSim <- function(iter_num=1000,age_range=c(20,45)){
+createEndoSim <- function(iter_num=1000,age_range=c(20,45), space_SI = 12){
+
+  suppressWarnings({
 
   simulation <- FUN.simulation3(ages = age_range[1]:age_range[2],
                                 number.of.cases=iter_num,
                                 aneuploid.table = Aneuploid_table_fitted_3day_210212,
-                                space.UI = 3, space.SI = 12, LB.adjust = F,
+                                space.UI = 3, space.SI = space_SI, LB.adjust = F,
                                 LB.adjust.risk= 0.02, PL.adjust = T, PL.adjust.risk= 0.04,
                                 reset.endo = F,PL.adjust.factor=10000,adjust.history = F)
+  })
 
-  new("EndoSim",iter_num=iter_num,age_range=age_range,result=simulation[[1]])
+  new("EndoSim",iter_num=iter_num,age_range=age_range,simulation=list(outcome=simulation[[1]],losses=simulation[[2]]))
 
 
 
@@ -49,8 +53,8 @@ createEndoSim <- function(iter_num=1000,age_range=c(20,45)){
 setMethod("show",
           "EndoSim",
           function(object) {
+            cat("EndoSim simulation object \n")
             cat("Number of iterations:",object@iter_num, "\n")
             cat("Age range:",object@age_range, "\n")
-            #cat("Contact:", object@contact, "\n")
           }
 )
